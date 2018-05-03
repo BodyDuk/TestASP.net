725266928ef0c9913e824ea1900667580a291639
BodanDemediuk <boda1000@gmail.com>
2018-05-03 11:01:02 +0300

    a

---
+++ FilmWebPortal/FilmWebPortal/Scripts/controller/filmController.js
@@ -0,0 +1,25 @@
+﻿function SaveFilm() {
+    var name = $('#Name').val();
+    var description = $('#Description').val();
+    var actorsList = [];
+    var inputElements = document.getElementsByClassName('messageCheckbox');
+    for (var i = 0; inputElements[i]; ++i) {
+        if (inputElements[i].checked) {
+            actorsList.push(inputElements[i].value);
+        }
+    }
+    $.ajax({
+        type: "POST",
+        cache: false,
+        url: "/Films/SaveFilm",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            Name: name,
+            Description: description,
+            SelectedActors: actorsList
+        }),
+        success: function () {
+            window.location.replace("https://localhost:44349/Films/Index");
+        },
+    });
+} 
---
+++ FilmWebPortal/FilmWebPortal/ViewModels/FilmViewModel.cs
@@ -0,0 +1,36 @@
+﻿using FilmWebPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FilmWebPortal.ViewModels
+{
+    public class FilmViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Назва")]
+        public string Name { get; set; }
+        [Display(Name = "Опис")]
+        public string Description { get; set; }
+        public ICollection<int> SelectedActors { get; set; } = new List<int>();
+        [Display(Name = "Дата виходу")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDay { get; set; }
+        public virtual ICollection<Actor> Actors { get; set; } = new List<Actor>();
+        public virtual IEnumerable<Actor> ActorsAll { get; set; } = new List<Actor>();        
+        public Director Director { get; set; }
+        public static explicit operator Film(FilmViewModel model)
+        {
+            return new Film {
+                Id=model.Id,
+                Name= model.Name,
+                Description=model.Description,
+                Actors = model.Actors,
+                Director=model.Director,
+                ReleaseDay=model.ReleaseDay
+            };
+        }
+    }
+}
